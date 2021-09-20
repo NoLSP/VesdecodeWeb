8fd196ef81460f4bfd869d9c16677369d10a1155
artem <artem@OFFICE.GENUM.RU>
2021-09-20 11:45:54 +0500

    edit1

--- Routes/routes.js
+++ Routes/routes.js
@@ -2,27 +2,27 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.render('index');
+    res.render('index.hbs');
 })
 
 router.get('/Task1', (req, res) => {
-    res.render('task1');
+    res.render('task1.hbs');
 })
 
 router.get('/Task2', (req, res) => {
-    res.render('task2');
+    res.render('task2.hbs');
 })
 
 router.get('/Task3', (req, res) => {
-    res.render('task3');
+    res.render('task3.hbs');
 })
 
 router.get('/Task4', (req, res) => {
-    res.render('task4');
+    res.render('task4.hbs');
 })
 
 router.get('/Task5', (req, res) => {
-    res.render('task5');
+    res.render('task5.hbs');
 })
 
 module.exports = router;
